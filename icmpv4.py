f59b7aa85aa1db306cc5a8d62c24cd556b5b99c3
Darktega <darktega@gmail.com>
2017-03-22 18:28:34 -0600

    Modified a typo in icmpv4.py rofl.

--- icmpv4.py
+++ icmpv4.py
@@ -1,5 +1,5 @@
 class ICMPv4:
-	"ICMP class, version 4"
+	'ICMP class, version 4'
 
 	def __init__(self, ipv4Payload):
 		_icmpType, _code, _checksum = struct.unpack( '! B B H', ipv4Payload[ : 4 ] )
